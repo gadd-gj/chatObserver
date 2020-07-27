32964b8960ab6155677b231417eeb040976bdec1
gustavR0 <gust040825@gmail.com>
2020-07-26 19:10:30 -0500

    whatsapp 2 :V

--- src/logico/Servidor.java
+++ src/logico/Servidor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package logico;
 
 import java.io.DataInputStream;
@@ -13,10 +8,6 @@ import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author gaddiel
- */
 public class Servidor extends Observable implements Runnable {
 
     private int puerto;
